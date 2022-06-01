858a4be4bc3f65972a2dd4dbbad4488192c1311d
phamtanphat <phatdroid94@gmail.com>
2022-06-01 20:12:11 +0700

    khoi tao stream

--- bin/asynchornized_22042022.dart
+++ bin/asynchornized_22042022.dart
@@ -2,16 +2,39 @@ import 'package:dio/dio.dart';
 import 'apiService.dart';
 
 void main(List<String> arguments) {
-  var option = BaseOptions(
-      connectTimeout: 30000,
-      receiveTimeout: 30000,
-      baseUrl: 'https://khoapham.vn/');
-  var dio = Dio(option);
+  // var option = BaseOptions(
+  //     connectTimeout: 30000,
+  //     receiveTimeout: 30000,
+  //     baseUrl: 'https://khoapham.vn/');
+  // var dio = Dio(option);
+  //
+  // var client = ApiService(dio);
+  //
+  // client
+  //     .getDemo3()
+  //     .then((value) => print(value.language?.en?.name))
+  //     .catchError((error) => print(error));
 
-  var client = ApiService(dio);
+  // Future
+  // var future = Future.value(5);
+  // var strings = Stream.fromFuture(future);
+  //
+  // strings.listen((event) {
+  //   print(event);
+  // });
 
-  client
-      .getDemo3()
-      .then((value) => print(value.language?.en?.name))
-      .catchError((error) => print(error));
+  // iterable
+  // var iterable = Iterable.generate(10,(index) => 'i + $index');
+  //
+  // var stream = Stream.fromIterable(iterable);
+  //
+  // stream.listen((event) {
+  //   print(event);
+  // });
+
+  // periodic
+  // var stream = Stream.periodic(Duration(seconds: 1), (index) => index);
+  // stream.take(20).listen((event) {
+  //   print(event);
+  // });
 }
